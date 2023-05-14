aa6ccd5c5e21f03d220763fbffa00aa2426b28f3
Abhay <abhaytyagi2513@gmail.com>
2023-05-14 23:06:51 +1000

    added a todo component

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,24 @@
 <template>
 	<div id="app">
 		<h1>To-Do List</h1>
+		<ul>
+			<li>
+				<to-do-item></to-do-item>
+			</li>
+		</ul>
 	</div>
 </template>
 
 <script>
 
+import ToDoItem from './components/ToDoItem.vue';
+
 export default {
-	name: 'App',
-}
+	name: "app",
+	components: {
+		ToDoItem,
+	},
+};
 </script>
 
 <style>
