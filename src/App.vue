b2004f368f74d42f96e6260a1efbf10c417b2206
Abhay <abhaytyagi2513@gmail.com>
2023-06-01 23:55:40 +1000

    commit 2

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <template>
 	<div id="app">
 		<h1>To-Do List</h1>
+		<to-do-form></to-do-form>>
 		<ul>
 			<li v-for="item in ToDoItems" :key="item.id">
 				<to-do-item :label="item.label" :done="true" :id="item.id"></to-do-item>
@@ -12,11 +13,13 @@
 <script>
 
 import ToDoItem from './components/ToDoItem.vue';
+import ToDoForm from './components/ToDoForm.vue';
 
 export default {
 	name: "app",
 	components: {
 		ToDoItem,
+		ToDoForm,
 	},
 	data() {
 		return {
